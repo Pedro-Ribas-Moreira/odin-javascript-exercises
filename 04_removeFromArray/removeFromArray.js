22c3e87bb3462a58fe8ba4bb31f9c9bf2d6f467c
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-02 08:32:51 +0000

    adding exercise 04-removing from array, using the test cases to create the optimal version of the function, and learning about mutiple parameters in a function, and how to deal with them with ..arg operator.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arg) {
+  let argArray = Array.from(arg);
+  console.log(argArray);
+  for (let i = 0; i < argArray.length; i++) {
+    const index = arr.indexOf(argArray[i]);
+    if (index !== null && index !== -1) {
+      arr.splice(index, 1);
+    }
+  }
 
+  console.log(arr);
+  return arr;
 };
+
+// removeFromArray([1, 2, 3, 4], 3, 2, "pedro", 5, "4");
 
 // Do not edit below this line
 module.exports = removeFromArray;
