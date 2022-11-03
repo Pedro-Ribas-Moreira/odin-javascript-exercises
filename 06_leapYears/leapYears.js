5bc327c3d1314412df33f1be2ec0794ebad4c379
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-03 08:14:12 +0000

    adding exercise 06, learning about nesting if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeap = true;
 
+  if (year % 4 !== 0) {
+    isLeap = false;
+  }
+  if (year % 100 === 0 && year % 400 !== 0) {
+    isLeap = false;
+  }
+
+  return isLeap;
 };
 
+leapYears(1900);
 // Do not edit below this line
 module.exports = leapYears;
