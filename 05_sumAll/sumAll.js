506300d003499397744a0d9e5c7d4638d22f814e
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-03 07:59:29 +0000

    added exercise 05, where I learned about assign variable values based on conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  }
+  let sum = 0;
+  let counter = num2 > num1 ? num1 : num2;
+  let length = num2 > num1 ? num2 : num1;
+  for (let i = counter; i <= length; i++) {
+    sum += i;
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
