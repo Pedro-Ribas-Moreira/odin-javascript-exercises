1e489280211ab84aa5c01ea98c4caf5891e88eab
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-03 08:21:07 +0000

    added exercise 07, learning how to search on google for formulas examples and to know the best answers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  let fTemp = fahrenheit;
+  let fToCel = ((fTemp - 32) * 5) / 9;
 
+  let result = Math.round(fToCel * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
+const ctof = function (celsius) {
+  var cTemp = celsius;
+  var cToFahr = (cTemp * 9) / 5 + 32;
+  let result = Math.round(cToFahr * 10) / 10;
 
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
