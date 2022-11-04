6dce98360f1358ca07b7c23553718e457dcf6596
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-04 19:26:14 +0000

    adding exercise 13, learning about encription

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,32 @@
-const caesar = function() {
+const caesar = function (message, n) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let newalpha = "";
+  for (let i = 0; i < alphabet.length; i++) {
+    let offset = (i + n) % alphabet.length;
+    if (offset < 0) {
+      offset = offset + alphabet.length;
+    }
+    newalpha += alphabet[offset];
+  }
+  console.log(newalpha);
 
+  let result = "";
+  for (let i = 0; i < message.length; i++) {
+    let index = alphabet.indexOf(message[i].toLowerCase());
+    if (index != -1) {
+      if (message[i] == message[i].toUpperCase()) {
+        result += newalpha[index].toUpperCase();
+      } else {
+        result += newalpha[index];
+      }
+    } else {
+      result += message[i];
+    }
+  }
+  return result;
 };
 
+console.log(caesar("Ab c!", 3));
+console.log(caesar("Ab c!", -3));
 // Do not edit below this line
 module.exports = caesar;
