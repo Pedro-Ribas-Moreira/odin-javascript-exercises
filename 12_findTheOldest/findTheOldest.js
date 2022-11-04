d8efa9d7754358b6073fc4baaa5ed1d1bf2b9d94
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-04 16:03:02 +0000

    iterating with objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldest;
+  people.forEach((person) => {
+    let age = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : parseInt(new Date().getFullYear()) - person.yearOfBirth;
 
+    console.log(age);
+    person["age"] = age;
+    console.log(person);
+    if (oldest == null) {
+      oldest = person;
+    } else if (oldest.age < person.age) {
+      oldest = person;
+    }
+  });
+
+  return oldest;
 };
 
+// console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
