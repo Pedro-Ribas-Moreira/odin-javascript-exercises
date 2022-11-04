5c5edfd4c0e673cb4fe1ac421d067f999ad9277a
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-04 13:55:59 +0000

    adding exercises with recursive functions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function (numInteractions) {
+  let num1 = 1;
+  let num2 = 1;
+  result = numInteractions;
+  for (let i = 2; i < numInteractions; i++) {
+    result = num1 + num2;
+    num1 = num2;
+    num2 = result;
+  }
+  return result;
 };
+
+console.log(fibonacci(600));
 
 // Do not edit below this line
 module.exports = fibonacci;
+/**
+ const fibonacci = n => {
+  if (n <= 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+
+};
+*/
