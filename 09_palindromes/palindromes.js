78022fa758388011086ad86e5c8f0c04490446bd
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-04 12:01:49 +0000

    adding exercise 09, learning how to manipulate strings and converting them in arrays.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let str1 = str
+    .replace(/[^\w " "]/g, "")
+    .split(" ")
+    .join("")
+    .toLowerCase();
+  let counter = str1.length;
+  let compareStr = [];
+  for (let i = counter; i >= 0; i--) {
+    compareStr.push(str1[i]);
+  }
 
+  return str1 === compareStr.join("") ? true : false;
 };
 
 // Do not edit below this line
